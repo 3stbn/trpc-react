936cc8d5c0e35ab8cef8648f8fbe46a4e678d0d1
3stbn <esteban93torres@gmail.com>
2023-01-01 15:37:27 -0500

    remove unnesesary things

--- shared/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.tutorialStatusEnum = void 0;
-exports.tutorialStatusEnum = [
-    "completed",
-    "inProgress",
-    "watchLater",
-];
