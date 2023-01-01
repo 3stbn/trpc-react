a56438c159eca1f53190b7a865e23c65b6c14109
3stbn <esteban93torres@gmail.com>
2023-01-01 15:31:42 -0500

    upgrade dependencies

--- client/src/utils/trpc.ts
+++ client/src/utils/trpc.ts
@@ -1,3 +1,3 @@
-import { createTRPCReact } from "@trpc/react";
+import { createTRPCReact } from "@trpc/react-query";
 import type { AppRouter } from "../../../server/index";
 export const trpc = createTRPCReact<AppRouter>();
